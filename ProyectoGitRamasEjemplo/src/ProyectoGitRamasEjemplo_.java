39461c4bc81c63cfe8ae0b6a6113d4c39e1f7aca
usuario <usuario@DESKTOP-5FAD0MS>
2022-11-22 23:19:16 +0100

    Cambio 3
    Se ha cambiado hello por saludos

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo_.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo_.java
@@ -5,7 +5,7 @@ public class ProyectoGitRamasEjemplo_ {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello world");
+		System.out.println("Saludos world");
 		
 		
 		public static void imprimeSaludo(){
